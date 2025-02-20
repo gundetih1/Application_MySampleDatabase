8ec03135e193bbf3cab08953f2b45f1f5be452a7
Harshavardhan Gundeti <gundeti.harsha@gmail.com>
2025-02-19 21:32:04 -0500

    changed request mapping to post mappinf for create customers

--- src/main/java/com/example/demo/Controller/CustomersController.java
+++ src/main/java/com/example/demo/Controller/CustomersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,7 @@ public class CustomersController {
 		return customersService.getCustomerById(id);
 	}
 	
-	@RequestMapping("/createcustomers")
+	@PostMapping("/createcustomers")
 	public List<Customers> createCustomers(@RequestBody List<Customers> customers){
 		System.out.println("Customers Created");
 		return customersService.saveCustomers(customers);
